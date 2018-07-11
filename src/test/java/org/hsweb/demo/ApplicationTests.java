44e929c7f0c62e4c6d4874d912d727d10b948bb9
zehua <523786283@qq.com>
2018-07-11 13:36:39 +0800

    集成redis

---
+++ src/test/java/org/hsweb/demo/ApplicationTests.java
@@ -0,0 +1,28 @@
+package org.hsweb.demo;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Run.class)
+public class ApplicationTests {
+
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+
+	@Test
+	public void test() throws Exception {
+
+		// 保存字符串
+		stringRedisTemplate.opsForValue().set("aaa", "111");
+		Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+
+    }
+
+}
